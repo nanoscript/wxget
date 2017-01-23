53864959f4cbfee385b96a746afcef733dfaf423
nanoscript <xclusive585@gmail.com>
2017-01-22 23:31:21 -0500

    Update wxget.sh
    made some usability changes

--- wxget.sh
+++ wxget.sh
@@ -1,9 +1,21 @@
-#!/bin/sh
-# --------------Special thanks to PhirePhly for cleaning this up a bit!
+#!/bin/sh                                                                                                                               
+
+
+# wxget, a simple command line utility for displaying your local weather on a command line output
+# The only requirements is that wget be installed on your system (it probably already is)
+####
 
 # Look up zone number here: http://www.nws.noaa.gov/mirs/public/prods/maps/pfzones_list.htm
+
+#state in lowercase!
+
 STATE=ny
 NOAAZONE=003
 
-NOAAURL="http://weather.noaa.gov/pub/data/forecasts/zone/$STATE/$STATE""z$NOAAZONE.txt"
-wget -q -O - $NOAAURL | less
+####
+ROUNDUP="http://tgftp.nws.noaa.gov/data/observations/state_roundup/$STATE/$STATE""z$NOAAZONE.txt"
+ZONE="http://tgftp.nws.noaa.gov/data/forecasts/zone/$STATE/$STATE""z$NOAAZONE.txt"
+
+clear;
+echo "CursorDown to continue, 'q' to quit"; sleep .9; clear;
+wget -q -O - $ROUNDUP $ZONE | less
